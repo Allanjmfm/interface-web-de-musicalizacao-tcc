eb10cfcef2d4824078c34a932ae0f89f235a32b7
Allan Mendes <104873193+Allanjmfm@users.noreply.github.com>
2023-03-06 11:53:54 -0300

    adição do botão contraste

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-console.log('teste');
+const darkMode=document.querySelector("#dark-mode");
+const contrast=document.querySelector("#dark-menu");
+
+function toggleDarkMode(){
+if (darkMode.classList.contains("dark-mode")){
+    console.log("alert");
+    darkMode.classList.remove("dark-mode");
+}else{
+    darkMode.classList.add("dark-mode");
+    }
+}
+
+contrast.addEventListener("click", toggleDarkMode());
