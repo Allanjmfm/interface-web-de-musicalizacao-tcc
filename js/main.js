340bd9a7ac8bd6c776264f2dd3418b0176bf1d6f
Allan Mendes <104873193+Allanjmfm@users.noreply.github.com>
2023-03-21 12:03:50 -0300

    criação dos scripts de trocas de conteúdo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
+// Variável dark mode
 const darkMode=document.querySelector("#dark-mode");
 const contrast=document.querySelector("#dark-menu");
 
+// Variável troca de conteúdo
+const selecaoEtapa=document.querySelector("#btn_sel_etapas");
+// const voltar=document.querySelector("#btn_voltar");
+
+// Dark Mode
 function toggleDarkMode(){
     darkMode.classList.toggle("dark-mode");
     document.querySelector("#sobre-menu").classList.toggle("letra-dark");
@@ -11,4 +17,31 @@ function toggleDarkMode(){
 
 contrast.addEventListener("click", toggleDarkMode());
 
+// Scripts Troca de conteúdo
+selecaoEtapa.addEventListener("click", (e)=>{
+    if(e.type == 'click'){
+        console.log(selecaoEtapa);
+        trocaConteudo("views/selecao_etapas.html");
+        // voltar=document.querySelector("#btn_voltar");
+    }
+});
+function voltar(){
+    
+    trocaConteudo("views/tela_inicial.html");
+        
+};
+
+// Botão Voltar tela inicial
+function trocaConteudo(arquivo) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            var response = xhr.responseText;
+            document.getElementById("conteudo").innerHTML = response;
+        }
+    };
+    xhr.open("GET", arquivo, true);
+    xhr.send();
+    return;
+}
 
