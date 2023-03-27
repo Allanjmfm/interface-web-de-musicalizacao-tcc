12e65773ae00e8178adc8352a0dabe3c889eb936
Allan Mendes <104873193+Allanjmfm@users.noreply.github.com>
2023-03-27 11:40:01 -0300

    Testes de funcionamento do botão de aumentar fonte

--- js/main.js
+++ js/main.js
@@ -17,61 +17,94 @@ function toggleDarkMode(){
 
 contrast.addEventListener("click", toggleDarkMode());
 
-// Scripts Troca de conteúdo
-// selecaoEtapa.addEventListener("click", (e)=>{
-//     if(e.type == 'click'){
-//         console.log(selecaoEtapa);
-//         trocaConteudo("views/selecao_etapas.html");
-//         console.log(e);
-//         // voltar=document.querySelector("#btn_voltar");
-//     }
-// });
-// function voltar(){
-    
-//     trocaConteudo("views/tela_inicial.html");
-        
-// };
-let telas =[{
-    "telaInicial": "/views/tela_inicial.html"},
-    {"selecaoEtapas": "/views/selecao_etapas.html"},
-    {"testeEtapa1" : "/views/etapa1/teste_etapa1.html"},
-    {"testeArray" : "/views/etapa1/teste_etapa1_1.html"}
-]
+var textIncreaseCount = 0;
 
-let voltar;
-let telaAnterior = null;
-// const telaInicial = "/views/tela_inicial.html"
-// const selecaoEtapas = "/views/selecao_etapas.html"
-// const testeEtapa1 = "/views/etapa1/teste_etapa1.html"
-
-
-trocaConteudo(telas[0].telaInicial);
-
-function btnSelEtapas(){
-    voltar = telas[0].telaInicial;
-    trocaConteudo(telas[1].selecaoEtapas);
+function increaseTextSize() {
+  var text = document.getElementById("text");
+  var currentSize = parseInt(window.getComputedStyle(text).getPropertyValue("font-size"));
+  var newSize = currentSize + 2;
+  
+  // Verifica se o texto já foi aumentado 3 vezes
+  if (textIncreaseCount >= 3) {
+    // Se sim, redefine o tamanho da fonte para o valor inicial (16 pixels)
+    text.style.fontSize = "16px";
+    // Zera a contagem
+    textIncreaseCount = 0;
+  } else {
+    // Se não, aumenta o tamanho da fonte normalmente
+    text.style.fontSize = newSize + "px";
+    // Incrementa a contagem
+    textIncreaseCount++;
+  }
 }
 
-function btnVoltar(){
-    trocaConteudo( voltar);
-    if(telaAnterior){
-        voltar = telaAnterior;
-        telaAnterior = telas.map((conteudo, index)=>{
-            if(telaAnterior==conteudo.selecaoEtapas){
-                return conteudo.telaInicial;
-            }
-        })
-        console.log(telaAnterior);
+let telas = {
+    telaInicial: "/views/tela_inicial.html",
+    selecaoEtapas: "/views/selecao_etapas.html",
+    testeEtapa1: "/views/etapa1/teste_etapa1.html",
+    testeArray: "/views/etapa1/teste_etapa1_1.html",
+  };
 
+  // Variável que receber tela anterior para ajudar
+  // o usuaro para navigar para tele anterior.
+  let voltar;
+  let telaAnterior = null;
+  
+  // const telaInicial = "/views/tela_inicial.html"
+  // const selecaoEtapas = "/views/selecao_etapas.html"
+  // const testeEtapa1 = "/views/etapa1/teste_etapa1.html"
+  
+
+  trocaConteudo(telas.telaInicial);
+  
+  function btnSelEtapas() {
+    voltar = telas.telaInicial;
+    trocaConteudo(telas.selecaoEtapas);
+  }
+  
+  //async function  Ela permite que você escreva código assíncrono
+  // em um estilo síncrono.
+  async function btnVoltar() {
+    // Chamamos a func Troca Conteudo com algumento variavel voltar.
+    // await para esperar que uma operação assíncrona termine
+    // antes de continuar a execução da função
+    await trocaConteudo(voltar);
+  
+    // Se variavel volata == telainicial então a tela anterior recebe null.
+    if (voltar == telas.telaInicial) {
+      telaAnterior = null;
+      return;
     }
-}
-function btnEtapa1(){
+    // aqui se tela anterior não é null.
+    if (telaAnterior) {
+      // variavel voltar receber tela anterior.
+      voltar = telaAnterior;
+  
+      // Coloquei "telas" em colchetes porque o a func map só funciona
+      // arry, `[]` representa um array vazio em JavaScript.
+      // Um array é uma estrutura de dados que pode armazenar
+      // múltiplos valores em uma única variável.
+      [telas].map(function (conteudo) {
+        // Aqui verifiquei se tela aterior se sim a tela anterior
+        // recebe endreço da tela inicial.
+        if (telaAnterior == conteudo.selecaoEtapas) {
+          telaAnterior = conteudo.telaInicial;
+          return;
+        } /* else if (telaAnterior == conteudo.selecaoEtapas) {
+          console.log(conteudo);
+          return conteudo.telaInicial;
+        } */
+      });
+      //console.log(telaAnterior);
+    }
+  }
+  function btnEtapa1() {
     telaAnterior = voltar;
-    voltar = telas[1].selecaoEtapas;
-    trocaConteudo(telas[2].testeEtapa1);
-}
-function btnTesteArray(){
+    voltar = telas.selecaoEtapas;
+    trocaConteudo(telas.testeEtapa1);
+  }
+  function btnTesteArray() {
     telaAnterior = voltar;
-    voltar = telas[2].testeEtapa1;
-    trocaConteudo(telas[3].testeArray);
-}
+    voltar = telas.testeEtapa1;
+    trocaConteudo(telas.testeArray);
+  }
