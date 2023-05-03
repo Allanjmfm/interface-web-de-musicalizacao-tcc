c2d613a19e8e51b6eabcdade5a0136609cfc637f
prinako <nyankonton122@gmail.com>
2023-05-03 11:34:14 -0300

    no fuc

--- assets/js/etapa1.js
+++ assets/js/etapa1.js
@@ -1,104 +1,112 @@
-let conteudo_etapa1 = {
-  conteudo1: "views/etapa1/etapa1_tela1.html",
-  conteudo2: "views/etapa1/etapa1_tela2.html",
-  exercicio1: "views/etapa1/etapa1_exercicio1.html",
-  exercicio2: "views/etapa1/etapa1_exercicio2.html",
-  exercicio3: "views/etapa1/etapa1_exercicio3.html",
-  exercicio4: "views/etapa1/etapa1_exercicio4.html",
-  exercicio5: "views/etapa1/etapa1_exercicio5.html",
-  exercicio6: "views/etapa1/etapa1_exercicio6.html",
-  exercicio7: "views/etapa1/etapa1_exercicio7.html",
-};
+let conteudo_etapa1 =[
+   {conteudo1: "views/etapa1/etapa1_tela1.html"},
+   {conteudo2: "views/etapa1/etapa1_tela2.html"},
+   {exercicio1: "views/etapa1/etapa1_exercicio1.html"},
+   {exercicio2: "views/etapa1/etapa1_exercicio2.html"},
+   {exercicio3: "views/etapa1/etapa1_exercicio3.html"},
+   {exercicio4: "views/etapa1/etapa1_exercicio4.html"},
+   {exercicio5: "views/etapa1/etapa1_exercicio5.html"},
+   {exercicio6: "views/etapa1/etapa1_exercicio6.html"},
+   {exercicio7: "views/etapa1/etapa1_exercicio7.html"},
+ 
+]
 
 const conteudoEtapa1 = "#conteudo-etapas";
 
 let voltarEtapa1;
-let conteudoAtual = conteudo_etapa1.conteudo1;
+let conteudoAtual = conteudo_etapa1[0].conteudo1;
 let proxConteudoEtapa1 = null;
 
 function conteudoInicial() {
-  trocaConteudo(conteudo_etapa1.conteudo1, conteudoEtapa1);
+  trocaConteudo(conteudo_etapa1[0].conteudo1, conteudoEtapa1);
   setTimeout(CheckDarkmode, 10);
   //   setTimeout(document.getElementById("btn-voltar-context").innerHTML = "Voltar", 10
   //   )
 }
-
-function btnAvancar() {
-  voltarEtapa1 = conteudoAtual;
-  [conteudo_etapa1].map(function (items) {
-    if (conteudoAtual == items.conteudo1) {
-      proxConteudoEtapa1 = items.conteudo2;
-      conteudoAtual = proxConteudoEtapa1;
-      return;
-    }
-    if (conteudoAtual == items.conteudo2) {
-      proxConteudoEtapa1 = items.exercicio1;
-      conteudoAtual = proxConteudoEtapa1;
-      return;
-    }
-    if (conteudoAtual == items.exercicio1) {
-      proxConteudoEtapa1 = items.exercicio2;
-      conteudoAtual = proxConteudoEtapa1;
-      return;
-    }
-    if (conteudoAtual == items.exercicio2) {
-      proxConteudoEtapa1 = items.exercicio3;
-      conteudoAtual = proxConteudoEtapa1;
-      return;
-    }
-    if (conteudoAtual == items.exercicio3) {
-      proxConteudoEtapa1 = items.exercicio4;
-      conteudoAtual = proxConteudoEtapa1;
-      return;
-    }
-    if (conteudoAtual == items.exercicio4) {
-      proxConteudoEtapa1 = items.exercicio5;
-      conteudoAtual = proxConteudoEtapa1;
-      return;
-    }
-    if (conteudoAtual == items.exercicio5) {
-      proxConteudoEtapa1 = items.exercicio6;
-      conteudoAtual = proxConteudoEtapa1;
-      return;
-    }
-    if (conteudoAtual == items.exercicio6) {
-      proxConteudoEtapa1 = items.exercicio7;
-      conteudoAtual = proxConteudoEtapa1;
-      return;
-    }
-    if (conteudoAtual == items.exercicio7) {
-      proxConteudoEtapa1; //items.TELA INCIAL ETAPA 2;
-    }
+function btnAvancar(proxIndex, arrayEtapas) {
+  console.log(arrayEtapas);
+  proxConteudoEtapa1 = arrayEtapas.map((items,index)=>{
+    if(proxIndex == index) return items
   });
-  trocaConteudoEtapas(proxConteudoEtapa1, conteudoEtapa1);
-  setTimeout(CheckDarkmode, 10);
-
-  //   Este trecho de código em JavaScript verifica se o conteúdo
-  //   atual da página é o exercício 7 da etapa 1. Se sim, ele atualiza
-  //   o texto do botão de avançar para "Etapa 2". Caso contrário, ele
-  //   atualiza o texto do botão de avançar para "Avançar".
-  //   A função que contém este trecho de código deve ser acionada quando o
-  //   usuário está navegando pela página e avança ou retorna para diferentes
-  //   conteúdos da etapa 1.
-  if (conteudoAtual == conteudo_etapa1.exercicio7) {
-    document.getElementById("btn-avancar-context").innerHTML = "Etapa 2";
-  } else {
-    document.getElementById("btn-avancar-context").innerHTML = "Avançar";
-  }
-
-  //   Este trecho de código em JavaScript verifica se o conteúdo atual da página
-  //   é o primeiro conteúdo da etapa 1. Se sim, ele atualiza o texto do botão de voltar para
-  //   "Voltar para Etapas". Caso contrário, ele atualiza o texto do botão de voltar para
-  //   "Voltar". A função que contém este trecho de código deve ser acionada quando o usuário
-  //   está navegando pela página e retorna para diferentes conteúdos da etapa 1.
-  if (conteudoAtual == conteudo_etapa1.conteudo1) {
-    //aqui para troca texto no botom de voltar se o usuario esta na tela de etapa1 tela1
-    document.getElementById("btn-voltar-context").innerHTML =
-      "Voltar para Etapas";
-  } else {
-    document.getElementById("btn-voltar-context").innerHTML = "Voltar";
-  }
+  console.log(proxConteudoEtapa1)
+  
 }
+// function btnAvancar() {
+//   voltarEtapa1 = conteudoAtual;
+//   [conteudo_etapa1].map(function (items) {
+//     if (conteudoAtual == items.conteudo1) {
+//       proxConteudoEtapa1 = items.conteudo2;
+//       conteudoAtual = proxConteudoEtapa1;
+//       return;
+//     }
+//     if (conteudoAtual == items.conteudo2) {
+//       proxConteudoEtapa1 = items.exercicio1;
+//       conteudoAtual = proxConteudoEtapa1;
+//       return;
+//     }
+//     if (conteudoAtual == items.exercicio1) {
+//       proxConteudoEtapa1 = items.exercicio2;
+//       conteudoAtual = proxConteudoEtapa1;
+//       return;
+//     }
+//     if (conteudoAtual == items.exercicio2) {
+//       proxConteudoEtapa1 = items.exercicio3;
+//       conteudoAtual = proxConteudoEtapa1;
+//       return;
+//     }
+//     if (conteudoAtual == items.exercicio3) {
+//       proxConteudoEtapa1 = items.exercicio4;
+//       conteudoAtual = proxConteudoEtapa1;
+//       return;
+//     }
+//     if (conteudoAtual == items.exercicio4) {
+//       proxConteudoEtapa1 = items.exercicio5;
+//       conteudoAtual = proxConteudoEtapa1;
+//       return;
+//     }
+//     if (conteudoAtual == items.exercicio5) {
+//       proxConteudoEtapa1 = items.exercicio6;
+//       conteudoAtual = proxConteudoEtapa1;
+//       return;
+//     }
+//     if (conteudoAtual == items.exercicio6) {
+//       proxConteudoEtapa1 = items.exercicio7;
+//       conteudoAtual = proxConteudoEtapa1;
+//       return;
+//     }
+//     if (conteudoAtual == items.exercicio7) {
+//       proxConteudoEtapa1; //items.TELA INCIAL ETAPA 2;
+//     }
+//   });
+//   trocaConteudoEtapas(proxConteudoEtapa1, conteudoEtapa1);
+//   setTimeout(CheckDarkmode, 10);
+
+//   //   Este trecho de código em JavaScript verifica se o conteúdo
+//   //   atual da página é o exercício 7 da etapa 1. Se sim, ele atualiza
+//   //   o texto do botão de avançar para "Etapa 2". Caso contrário, ele
+//   //   atualiza o texto do botão de avançar para "Avançar".
+//   //   A função que contém este trecho de código deve ser acionada quando o
+//   //   usuário está navegando pela página e avança ou retorna para diferentes
+//   //   conteúdos da etapa 1.
+//   if (conteudoAtual == conteudo_etapa1.exercicio7) {
+//     document.getElementById("btn-avancar-context").innerHTML = "Etapa 2";
+//   } else {
+//     document.getElementById("btn-avancar-context").innerHTML = "Avançar";
+//   }
+
+//   //   Este trecho de código em JavaScript verifica se o conteúdo atual da página
+//   //   é o primeiro conteúdo da etapa 1. Se sim, ele atualiza o texto do botão de voltar para
+//   //   "Voltar para Etapas". Caso contrário, ele atualiza o texto do botão de voltar para
+//   //   "Voltar". A função que contém este trecho de código deve ser acionada quando o usuário
+//   //   está navegando pela página e retorna para diferentes conteúdos da etapa 1.
+//   if (conteudoAtual == conteudo_etapa1.conteudo1) {
+//     //aqui para troca texto no botom de voltar se o usuario esta na tela de etapa1 tela1
+//     document.getElementById("btn-voltar-context").innerHTML =
+//       "Voltar para Etapas";
+//   } else {
+//     document.getElementById("btn-voltar-context").innerHTML = "Voltar";
+//   }
+// }
 
 // A função "voltarConteudoEtapa1" é um trecho de código em JavaScript que atualiza
 // o conteúdo e o botão de volta de uma página da web quando o usuário clica no botão
