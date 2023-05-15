303765c5f13a832f1bdeb4aa5551f9dc9b16ecf0
Allan Mendes <104873193+Allanjmfm@users.noreply.github.com>
2023-05-15 11:47:05 -0300

    implementação etapa 3 - falta exercicio

--- assets/js/etapa3.js
+++ assets/js/etapa3.js
@@ -4,5 +4,11 @@ let conteudo_etapa3 = {
     etapa3Conteudo3: "views/etapa3/etapa3_tela3.html",
     etapa3Conteudo4: "views/etapa3/etapa3_tela4.html",
     etapa3Conteudo5: "views/etapa3/etapa3_tela5.html",
-    etapa3Conteudo6: ""
+    etapa3Conteudo6: "views/etapa3/etapa3_tela6.html",
+    etapa3Conteudo7: "views/etapa3/etapa3_tela7.html",
+    etapa3Conteudo8: "views/etapa3/etapa3_tela8.html",
+    etapa3Conteudo9: "views/etapa3/etapa3_tela9.html",
+    etapa3Conteudo10: "views/etapa3/etapa3_tela10.html",
+    etapa3Conteudo11: "views/etapa3/etapa3_tela11.html",
+    etapa3Conteudo12: "views/etapa3/etapa3_tela12.html"
   };
