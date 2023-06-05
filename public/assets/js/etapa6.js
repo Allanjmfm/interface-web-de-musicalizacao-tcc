b6170996c733cdfc89b780e6f0ce04adb5218ed9
Allan Mendes <104873193+Allanjmfm@users.noreply.github.com>
2023-06-05 11:56:06 -0300

    finalização da etapa 6

--- public/assets/js/etapa6.js
+++ public/assets/js/etapa6.js
@@ -12,4 +12,13 @@ let conteudo_etapa6 = {
     etapa6Conteudo12: "etapa6_tela12.html",
     etapa6Conteudo13: "etapa6_tela13.html",
     etapa6Conteudo14: "etapa6_tela14.html",
+    etapa6Conteudo15: "etapa6_tela15.html",
+    etapa6Conteudo16: "etapa6_tela16.html",
+    etapa6Conteudo17: "etapa6_exercicio1.html",
+    etapa6Conteudo18: "etapa6_exercicio2.html",
+    etapa6Conteudo19: "etapa6_exercicio3.html",
+    etapa6Conteudo20: "etapa6_exercicio4.html",
+    etapa6Conteudo21: "etapa6_exercicio5.html",
+    etapa6Conteudo22: "etapa6_exercicio6.html",
+    etapa6Conteudo23: "etapa6_parabens.html"
 };
