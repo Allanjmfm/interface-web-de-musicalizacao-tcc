0c910b97e021fcafdbd1c5550d4dd00af1cdf12c
prinako <nyankonton122@gmail.com>
2023-08-16 10:43:44 -0300

    comentarios

--- public/assets/js/troca-conteudo.js
+++ public/assets/js/troca-conteudo.js
@@ -1,29 +1,31 @@
+// Função que chama a função trocaConteudo com os argumentos passados
 function trocaConteudoEtapas(proxConteudo, id) {
     trocaConteudo(proxConteudo, id);
 }
 
-// Script Troca de Conteúdo
+// Script para troca de conteúdo
 async function trocaConteudo(arquivo, id, cb) {
     let xhttp;
-    // const mainContainer = document.querySelector('#main-container');
+    // Obtém o elemento do DOM com o id especificado
     const mainContainer = document.querySelector(id);
-    // arquivo
-    //  = mainContainer.getAttribute("conteudo");
+
     if (arquivo) {
         xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
+                // Define o conteúdo do elemento com a resposta do servidor
                 mainContainer.innerHTML = this.responseText;
-                // mainContainer.removeAttribute("conteudo");
+                // Chama a função trocaConteudo passando a função de retorno como argumento
                 trocaConteudo(cb);
             }
         }
         xhttp.open("GET", arquivo, true);
         xhttp.send();
-        return cb() ? cd : "";
+        return cb() ? cb : ""; // Retorna a função de retorno ou uma string vazia
     }
+
+    // Se uma função de retorno (callback) for fornecida, executa-a
     if (cb) {
-        alert("lodfjoe")
         cb();
     }
 }
