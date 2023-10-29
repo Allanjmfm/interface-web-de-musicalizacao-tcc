e6614216a8039e354aee63ecbda667270c8a25df
prinako <nyankonton122@gmail.com>
2023-10-29 19:20:07 -0300

    adicionei progress no localStore

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -113,59 +113,58 @@ async function btnEtapa(linkTelaInicialEtapas) {
 
 // Função da alternativa correta nos exercícios
 function otrec(proxConteudo, index, id) {
-    // Seleciona o elemento com a classe "otrec" (resposta correta) e todos os elementos com a classe "odarre" (respostas erradas)
-    const respCertaDiv = document.querySelector(".otrec");
-    const respErradaDiv = document.querySelectorAll(".odarre");
-  
-    // Seleciona o elemento com o ID "mensagem"
-    const div = document.getElementById("mensagem");
-  
-    // Verifica se o elemento não tem a classe "msg-certa-cor"
-    if (!div.classList.contains("msg-certa-cor")) {
-      // Remove a classe "msg-incorrta-cor" se estiver presente
-      if (div.classList.remove("msg-incorrta-cor")) {
-        div.classList.remove("msg-incorrta-cor");
-      }
-      // Adiciona a classe "msg-certa-cor"
-      div.classList.add("msg-certa-cor");
+  // Seleciona o elemento com a classe "otrec" (resposta correta) e todos os elementos com a classe "odarre" (respostas erradas)
+  const respCertaDiv = document.querySelector(".otrec");
+  const respErradaDiv = document.querySelectorAll(".odarre");
+
+  // Seleciona o elemento com o ID "mensagem"
+  const div = document.getElementById("mensagem");
+
+  // Verifica se o elemento não tem a classe "msg-certa-cor"
+  if (!div.classList.contains("msg-certa-cor")) {
+    // Remove a classe "msg-incorrta-cor" se estiver presente
+    if (div.classList.remove("msg-incorrta-cor")) {
+      div.classList.remove("msg-incorrta-cor");
     }
-  
-    try {
-      // Adiciona classes aos elementos de resposta certa e desabilita-os
-      respCertaDiv.classList.add("btn-resp-certa");
-      respCertaDiv.classList.add("disabled-btn");
-  
-      // Itera sobre os elementos de resposta errada e os desabilita
-      Array.from(respErradaDiv).map((e) => e.classList.add("disabled-btn"));
-    } catch {}
-  
-    // Define o conteúdo do elemento "mensagem"
-    div.innerHTML = "Resposta Certa. Mandou bem.";
-  
-    // Atualiza a barra de progresso com base no índice
-    progressBar(index);
-  
-    // Aguarda 1 segundo (1000 milissegundos) antes de executar algumas ações
-    setTimeout(async () => {
-      // Aguarda a troca de conteúdo
-      await trocaConteudo(proxConteudo, id ? id : idConteudoEtapas);
-  
-      // Aguarda a atualização do link de conteúdo
-      await linkAtualDoConteudo(proxConteudo);
-  
-      // Aguarda 10 milissegundos antes de executar CheckDarkmode
-      setTimeout(CheckDarkmode, 10);
-    }, 1000);
-  
-    // Se o número de tentativas for menor que 2, atualize-o para 2
-    if (tentativas < 2) {
-      tentativas = 2;
-    }
+    // Adiciona a classe "msg-certa-cor"
+    div.classList.add("msg-certa-cor");
   }
-  
+
+  try {
+    // Adiciona classes aos elementos de resposta certa e desabilita-os
+    respCertaDiv.classList.add("btn-resp-certa");
+    respCertaDiv.classList.add("disabled-btn");
+
+    // Itera sobre os elementos de resposta errada e os desabilita
+    Array.from(respErradaDiv).map((e) => e.classList.add("disabled-btn"));
+  } catch {}
+
+  // Define o conteúdo do elemento "mensagem"
+  div.innerHTML = "Resposta Certa. Mandou bem.";
+
+  // Atualiza a barra de progresso com base no índice
+  progressBar(index);
+
+  // Aguarda 1 segundo (1000 milissegundos) antes de executar algumas ações
+  setTimeout(async () => {
+    // Aguarda a troca de conteúdo
+    await trocaConteudo(proxConteudo, id ? id : idConteudoEtapas);
+
+    // Aguarda a atualização do link de conteúdo
+    await linkAtualDoConteudo(proxConteudo);
+
+    // Aguarda 10 milissegundos antes de executar CheckDarkmode
+    setTimeout(CheckDarkmode, 10);
+  }, 1000);
+
+  // Se o número de tentativas for menor que 2, atualize-o para 2
+  if (tentativas < 2) {
+    tentativas = 2;
+  }
+}
 
 // Função da alternativa incorreta nos exercícios
-async function odarre(revisaoEtapa) {
+async function odarre(revisaoEtapa, index) {
   const respCerta = document.querySelector(".otrec");
   const respErrada = document.querySelectorAll(".odarre");
 
@@ -181,24 +180,33 @@ async function odarre(revisaoEtapa) {
   if (tentativas != 1) {
     div.innerHTML = "";
 
-    tentativas = tentativas - 1;
+    tentativas -= 1;
 
     div.innerHTML = `Resposta Incorreta. Tente novamente. ${tentativas} tentativa restante`;
   } else {
     tentativas = 2;
-    
+
     div.innerHTML =
       "Que pena, você errou todas as tentativas. Vamos recomeçar os estudos desta etapa.";
 
     setTimeout(() => {
-      recomecaEtapa(revisaoEtapa);
+      recomecaEtapa(revisaoEtapa, index);
     }, 3000);
   }
 }
 
 // Função para redirecionar o usuário para recomeçar a etapa
-function recomecaEtapa(link) {
+/**
+ * Reinicia a etapa atual removendo a página atual e redirecionando para um novo link.
+ *
+ * @param {string} link - O link para redirecionar.
+ * @returns {void}
+ */
+function recomecaEtapa(link, index) {
   removePaginaAtual();
+  if (index) {
+    reduceUserProgress(index);
+  }
   location.replace(origin + link);
 }
 
@@ -225,6 +233,11 @@ async function btnVoltarEtapas(voltar, id) {
   await setTimeout(CheckDarkmode, 10);
 }
 
+/**
+ * Amplia o zoom do corpo do documento ao aumentar o nível de zoom.
+ *
+ * @returns {void}
+ */
 function zoomIn() {
   if (zoomLevel >= 200) {
     return;
@@ -234,6 +247,11 @@ function zoomIn() {
   document.body.style.zoom = zoomLevel.toString() + "%";
 }
 
+/**
+ * Diminui o zoom do corpo do documento ao diminuir o nível de zoom.
+ *
+ * @returns {void}
+ */
 function zoomOut() {
   if (zoomLevel <= 70) {
     return;
@@ -243,118 +261,137 @@ function zoomOut() {
   document.body.style.zoom = zoomLevel.toString() + "%";
 }
 
+/**
+ * Atualiza a barra de progresso com base no índice e valor de progresso fornecidos.
+ *
+ * @param {number} index - O índice usado para determinar o aumento do progresso.
+ * @returns {void}
+ */
 function progressBar(index) {
-    index -=1;
-  // Definindo um array de cores para cada nível de progresso
-  const arrayColor = [
-    {
-      1: "#E7322A",
-      2: "#FF9B9B",
-      // 3:  '10'
-    },
-    {
-      1: "#F06222",
-      2: "#F4926483",
-      // 3:  '20'
-    },
-    {
-      1: "#F59421",
-      2: "#f8b463",
-      // 3:  '30'
-    },
-    {
-      1: "#e6cc00",
-      2: "#eddb4c",
-      // 3:  '40'
-    },
-    {
-      1: "#C0CA35",
-      2: "#d2d971",
-      // 3:  '50'
-    },
-    {
-      1: "#7DB341",
-      2: "#a4c97a",
-      // 3:  '60'
-    },
-    {
-      1: "#41A24A",
-      2: "#7abd80",
-      // 3:  '70'
-    },
-    {
-      1: "#1D988D",
-      2: "#60b6af",
-      // 3:  '80'
-    },
-    {
-      1: "#0FAAC3",
-      2: "#57c3d5",
-      // 3:  '90'
-    },
-    {
-      1: "#2591C2",
-      2: "#66b2d4",
-      // 3:  '100'
-    },
-  ];
+  index -= 1;
 
-  // Obtém o valor de progresso atual a partir de uma variável CSS usando getPropertyValue
-  let progressValue = Number(
-    trocaColor.getPropertyValue("--progress-value-root")
-  );
-
-//   console.log(arrayColor[index]);
+  // Obtém o valor de progresso atual do localStorage
+  let progressValue = Number(localStorage.getItem("userProgress"));
 
   // Verifica se progressValue é um número
-  if (typeof progressValue == "number") {
-    // Atualiza o progressValue com base no índice fornecido e em limites de progresso
-    // Cada índice corresponde a uma quantidade diferente de aumento de progresso
-    // Além disso, verifica se progressValue não ultrapassa os limites especificados
-    if (index == 0 && progressValue <= 10) {
+  if (typeof progressValue === "number") {
+    // Atualiza o progressValue com base no índice fornecido e nos limites de progresso
+    if (index === 0 && progressValue <= 10) {
       progressValue += 1.11111;
     }
-    if (index == 1 && progressValue <= 20) {
+    if (index === 1 && progressValue <= 20) {
       progressValue += 0.83333;
     }
-    if (index == 2 && progressValue <= 30) {
+    if (index === 2 && progressValue <= 30) {
       progressValue += 0.38462;
     }
-    if (index == 3 && progressValue <= 40) {
+    if (index === 3 && progressValue <= 40) {
       progressValue += 1.42857;
     }
-    if (index == 4 && progressValue <= 50) {
+    if (index === 4 && progressValue <= 50) {
       progressValue += 3.33333;
     }
-    if (index == 5 && progressValue <= 60) {
+    if (index === 5 && progressValue <= 60) {
       progressValue += 0.45455;
     }
-    if (index == 6 && progressValue <= 70) {
+    if (index === 6 && progressValue <= 70) {
       progressValue += 0.33333;
     }
-    if (index == 7 && progressValue <= 80) {
+    if (index === 7 && progressValue <= 80) {
       progressValue += 0.33333;
     }
-    if (index == 8 && progressValue <= 90) {
+    if (index === 8 && progressValue <= 90) {
       progressValue += 0.21739;
     }
-    if (index == 9 && progressValue <= 100) {
+    if (index === 9 && progressValue <= 100) {
       progressValue += 0.20833;
     }
 
-    // console.log(progressValue);
-
-    // Arredonda o valor do progresso para um número inteiro
-    root.style.setProperty(
-      "--progress-value-root",
-      "" + Math.floor(progressValue)
-    );
-
-    // Define as cores da barra de progresso e do contêiner com base no índice
-    root.style.setProperty("--progress-color", arrayColor[index][1]);
-    root.style.setProperty(
-      "--progress-container-bg-color",
-      arrayColor[index][2]
-    );
+    // Chama a função updateProgressBar para atualizar a barra de progresso
+    updateProgressBar(index, progressValue);
   }
+}
+
+/**
+ * Atualiza a barra de progresso e suas cores com base no índice e valor de progresso fornecidos.
+ *
+ * @param {number} index - O índice usado para determinar as cores da barra de progresso e do contêiner.
+ * @param {number} progressValue - O valor do progresso a ser exibido na barra de progresso.
+ * @returns {void}
+ */
+function updateProgressBar(index, progressValue) {
+  /**
+   * Um array de valores de cores para diferentes valores de índice.
+   * Cada valor de índice corresponde a uma cor específica para a barra de progresso e o contêiner.
+   * O array é estruturado da seguinte forma:
+   * [
+   *   { 1: "#cor1", 2: "#cor2" },
+   *   { 1: "#cor1", 2: "#cor2" },
+   *   ...
+   * ]
+   */
+  const arrayColor = [
+    // Cores para o índice 1
+    {
+      1: "#E7322A",
+      2: "#FF9B9B",
+    },
+    // Cores para o índice 2
+    {
+      1: "#F06222",
+      2: "#F4926483",
+    },
+    // Cores para o índice 3
+    {
+      1: "#F59421",
+      2: "#f8b463",
+    },
+    // Cores para o índice 4
+    {
+      1: "#e6cc00",
+      2: "#eddb4c",
+    },
+    // Cores para o índice 5
+    {
+      1: "#C0CA35",
+      2: "#d2d971",
+    },
+    // Cores para o índice 6
+    {
+      1: "#7DB341",
+      2: "#a4c97a",
+    },
+    // Cores para o índice 7
+    {
+      1: "#41A24A",
+      2: "#7abd80",
+    },
+    // Cores para o índice 8
+    {
+      1: "#1D988D",
+      2: "#60b6af",
+    },
+    // Cores para o índice 9
+    {
+      1: "#0FAAC3",
+      2: "#57c3d5",
+    },
+    // Cores para o índice 10
+    {
+      1: "#2591C2",
+      2: "#66b2d4",
+    },
+  ];
+
+  // Arredonda o valor do progresso para um número inteiro
+  root.style.setProperty(
+    "--progress-value-root",
+    "" + Math.floor(progressValue)
+  );
+
+  // Define as cores da barra de progresso e do contêiner com base no índice
+  root.style.setProperty("--progress-color", arrayColor[index][1]);
+  root.style.setProperty("--progress-container-bg-color", arrayColor[index][2]);
+
+  updateUserProgressInLocalStorage(index, progressValue);
 }
